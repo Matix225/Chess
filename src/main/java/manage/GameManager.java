e922ecb1ef5eb47a443124d10f0da1367e05d26f
203230 <s203230@student.pg.edu.pl>
2026-05-11 23:24:44 +0200

    Change visualization of the board Add knight class and his simple movement Add ability to move pawns by player

--- src/main/java/manage/GameManager.java
+++ src/main/java/manage/GameManager.java
@@ -1,7 +1,6 @@
 package manage;
 import pieces.*;
 import player.Human;
-
 import java.util.Random;
 
 public class GameManager {
@@ -30,6 +29,11 @@ public class GameManager {
         /*for(int i = 0; i < width; i++){
             //board[6][i] = new Pawn(6, i, Color.WHITE, '♙', this);
         }*/
+
+        //create knights
+        board[7][1] = new Knight(7, 1, Color.WHITE, '♘', this);
+        board[7][6] = new Knight(7, 6, Color.WHITE, '♘', this);
+
         board[7][4] = new King(7, 4, Color.WHITE, '♔', this);
     }
     public void createBlackPiece(){
@@ -37,6 +41,10 @@ public class GameManager {
         /*for(int i = 0; i < width; i++){
             board[1][i] = new Pawn(1, i, Color.BLACK, '♟', this);
         }*/
+
+        //create knights
+        board[0][1] = new Knight(0, 1, Color.BLACK, '♞', this);
+        board[0][6] = new Knight(0, 6, Color.BLACK, '♞', this);
 
         //create king
         board[0][4] = new King(0, 4, Color.BLACK, '♚', this);
@@ -52,14 +60,22 @@ public class GameManager {
     public Piece getPiece(int x, int y){
         return board[x][y];
     }
+    public void setPiece(Piece p, int x, int y){
+        board[p.getX()][p.getY()] = null;
+        p.setX(x);
+        p.setY(y);
+        board[x][y] = p;
+    }
 
     //game logic
     public void startGame(){
-        printBoard();
-        pl.makeMove();
+        while(true){
+            printBoard();
+            pl.makeMove();
+        }
     }
     public void printBoard(){
-        if(pl.getPieceColor() == Color.WHITE){
+        if(pl.getPieceColor() == Color.BLACK){
             for(int i = 0; i < 8; i++){
                 for(int j = 0; j < 8; j++){
                     if(board[i][j] == null)
@@ -73,6 +89,7 @@ public class GameManager {
         else{
             for(int i = 7; i >= 0; i--){
                 for(int j = 7; j >= 0; j--){
+                    boolean whiteSquare = (i + j) % 2 == 0;
                     if(board[i][j] == null)
                         System.out.print(" ");
                     else
